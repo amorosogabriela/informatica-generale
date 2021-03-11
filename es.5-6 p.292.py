0b85c559c413391eaea05a76b3092f36c74e23c8
amorosogabriela <74314806+amorosogabriela@users.noreply.github.com>
2021-03-11 10:23:07 +0100

    Update es.5-6 p.292.py

--- es.5-6 p.292.py
+++ es.5-6 p.292.py
@@ -1,7 +1,7 @@
-#esercizio_5 = Elenca propietà e metodi della classe prodotto.
-#esercizio_6 = Definisci il metodo assegna_prezzo della classe prodotto.
+#esercizio_5 = Elenca propietà e metodi della classe Prodotto.
+#esercizio_6 = Definisci il metodo assegna_prezzo della classe Prodotto.
 
-class prodotto:
+class Prodotto:
     def __init__(self, nome,  numero, provenienza, prezzo):
         self.nome = nome
         self.numero = numero
@@ -13,6 +13,6 @@ class prodotto:
     def info(self):
         print("il prodotto", self.nome, "ha un prezzo di", self.prezzo, "euro.")
 
-p1 = prodotto("tavolo", 2,"Italia", 220)
+p1 = Prodotto("tavolo", 2,"Italia", 220)
 p1.assegna_prezzo()
 p1.info()
